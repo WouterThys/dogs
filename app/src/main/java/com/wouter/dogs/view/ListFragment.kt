6f671a32f3b42a92f1c9e5ce12748dd0b4aaea19
Wouter <thys_wouter@hotmail.com>
2019-12-25 12:31:44 +0100

    initial commit

---
+++ app/src/main/java/com/wouter/dogs/view/ListFragment.kt
@@ -0,0 +1,16 @@
+package com.wouter.dogs.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.wouter.dogs.R
+
+class ListFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_list, container, false)
+    }
+
+}

2661c0ee1f4f43fb73a0e0e0b3966a6c04a20de7
Wouter <thys_wouter@hotmail.com>
2019-12-26 16:50:17 +0100

    palette

--- app/src/main/java/com/wouter/dogs/view/DetailFragment.kt
+++ app/src/main/java/com/wouter/dogs/view/DetailFragment.kt
@@ -1,6 +1,8 @@
 package com.wouter.dogs.view
 
 
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,8 +11,13 @@ import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.palette.graphics.Palette
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.transition.Transition
 import com.wouter.dogs.R
 import com.wouter.dogs.databinding.FragmentDetailBinding
+import com.wouter.dogs.model.DogPalette
 import com.wouter.dogs.viewmodel.DetailViewModel
 
 class DetailFragment : Fragment() {
@@ -21,7 +28,7 @@ class DetailFragment : Fragment() {
     private lateinit var binding : FragmentDetailBinding
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        binding = DataBindingUtil.inflate<FragmentDetailBinding>(inflater, R.layout.fragment_detail, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_detail, container, false)
         return binding.root
     }
 
@@ -42,7 +49,28 @@ class DetailFragment : Fragment() {
         viewModel.dogLiveData.observe(this, Observer { dog ->
             dog?.let {
                 binding.dog = it
+                it.imageUrl?.let { url -> setupBackgroundColor(url) }
             }
         })
+    }
+
+    private fun setupBackgroundColor(url: String) {
+        Glide.with(this)
+            .asBitmap()
+            .load(url)
+            .into(object: CustomTarget<Bitmap>() {
+                override fun onLoadCleared(placeholder: Drawable?) {
+
+                }
+
+                override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
+                    Palette.from(resource)
+                        .generate { palette ->
+                            val intColor = palette?.vibrantSwatch?.rgb ?: 0
+                            val myPalette = DogPalette(intColor)
+                            binding.palette = myPalette
+                        }
+                }
+            })
     }
 }

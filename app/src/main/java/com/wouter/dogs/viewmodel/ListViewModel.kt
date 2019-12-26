20996bfcf7a783f2dcf4feecf9e6934ccf9f39f9
Wouter <thys_wouter@hotmail.com>
2019-12-26 14:50:46 +0100

    room

--- app/src/main/java/com/wouter/dogs/viewmodel/ListViewModel.kt
+++ app/src/main/java/com/wouter/dogs/viewmodel/ListViewModel.kt
@@ -1,15 +1,17 @@
 package com.wouter.dogs.viewmodel
 
+import android.app.Application
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
 import com.wouter.dogs.model.DogBreed
+import com.wouter.dogs.model.DogDatabase
 import com.wouter.dogs.model.DogsApiService
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.observers.DisposableSingleObserver
 import io.reactivex.schedulers.Schedulers
+import kotlinx.coroutines.launch
 
-class ListViewModel : ViewModel() {
+class ListViewModel(application : Application) : BaseViewModel(application) {
 
     private val dogsService = DogsApiService()
     private val disposable = CompositeDisposable()
@@ -30,9 +32,7 @@ class ListViewModel : ViewModel() {
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribeWith(object: DisposableSingleObserver<List<DogBreed>>() {
                     override fun onSuccess(dogList: List<DogBreed>) {
-                        dogs.value = dogList
-                        dogsLoadError.value = false
-                        loading.value = false
+                        storeDogsLocally(dogList)
                     }
 
                     override fun onError(e: Throwable) {
@@ -41,6 +41,26 @@ class ListViewModel : ViewModel() {
                         e.printStackTrace()
                     }
                 }))
+    }
+
+    private fun dogsRetrieved(dogList : List<DogBreed>) {
+        dogs.value = dogList
+        dogsLoadError.value = false
+        loading.value = false
+    }
+
+    private fun storeDogsLocally(dogList : List<DogBreed>) {
+        launch {
+            val dao = DogDatabase(getApplication()).dogDao()
+            dao.deleteAllDogs()
+            val result = dao.insertAll(*dogList.toTypedArray())
+            var i = 0
+            while (i < dogList.size) {
+                dogList[i].uuid = result[i].toInt()
+                ++i
+            }
+            dogsRetrieved(dogList)
+        }
     }
 
     override fun onCleared() {
